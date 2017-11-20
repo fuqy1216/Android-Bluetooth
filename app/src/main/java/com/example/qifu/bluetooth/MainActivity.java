5cebe0c8f44c611e52ff255537278cd1d9c7b8a9
Qianyi Fu <qifu@umich.edu>
2017-11-20 11:11:06 -0500

    Add saving file funciton

--- app/src/main/java/com/example/qifu/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/qifu/bluetooth/MainActivity.java
@@ -2,16 +2,26 @@ package com.example.qifu.bluetooth;
         import android.bluetooth.BluetoothAdapter;
         import android.bluetooth.BluetoothDevice;
         import android.bluetooth.BluetoothSocket;
+        import android.bluetooth.BluetoothManager;
         import android.content.Intent;
         import android.os.Bundle;
+        import android.os.Environment;
         import android.os.Handler;
+        import android.util.Log;
         import android.view.View;
         import android.widget.TextView;
         import android.widget.EditText;
         import android.widget.Button;
+
+        import java.io.BufferedWriter;
+        import java.io.File;
+        import java.io.FileWriter;
         import java.io.IOException;
         import java.io.InputStream;
         import java.io.OutputStream;
+        import java.text.SimpleDateFormat;
+        import java.util.Date;
+        import java.util.Locale;
         import java.util.Set;
         import java.util.UUID;
 import android.support.v7.app.AppCompatActivity;
@@ -84,32 +94,35 @@ public class MainActivity extends AppCompatActivity {
 
     void findBT()
     {
-
-
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(getApplicationContext().BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        //mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(mBluetoothAdapter == null)
         {
             myLabel.setText("No bluetooth adapter available");
         }
-        if(!mBluetoothAdapter.isEnabled())
-        {
+        if(!mBluetoothAdapter.isEnabled()) {
             Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBluetooth, 0);
         }
-
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        String devicename = new String();;
         if(pairedDevices.size() > 0)
         {
             for(BluetoothDevice device : pairedDevices)
             {
-                if(device.getName().equals("MattsBlueTooth"))
+               // myLabel.setText(device.getName());
+                if(true/*device.getName().equals("SmartWatch 3 731C")*/)
                 {
                     mmDevice = device;
+                    devicename = device.getName();
                     break;
                 }
             }
         }
-        myLabel.setText("Bluetooth Device Found");
+        String found= "Bluetooth Device Found: " + devicename;
+        myLabel.setText(found);
     }
 
     void openBT() throws IOException
@@ -153,14 +166,58 @@ public class MainActivity extends AppCompatActivity {
                                 {
                                     byte[] encodedBytes = new byte[readBufferPosition];
                                     System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
-                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    final String data = new String(encodedBytes);
                                     readBufferPosition = 0;
-
                                     handler.post(new Runnable()
                                     {
                                         public void run()
                                         {
-                                            myLabel.setText(data);
+                                           myLabel.setText(data);
+
+                                            final int total_row = data.length();
+                                            Log.i("BlueToothAlbert", "total_row = " + total_row);
+                                            final String fileprefix = "export";
+                                            final String date = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.getDefault()).format(new Date());
+                                            final String filename = String.format("%s_%s.txt", fileprefix);
+
+                                            // final String directory = getContext().getApplicationContext().getFilesDir().getAbsolutePath();// + "/Albert";
+                                            final String directory = Environment.getExternalStorageDirectory().getAbsolutePath();// + "/sdcard";
+                                            final String direc = "/storage/emulated/0";
+                                            final File logfile = new File(directory, filename);
+                                            final File logPath = logfile.getParentFile();
+
+                                            if (!logPath.isDirectory() && !logPath.mkdirs()) {
+                                                Log.e("BlueToothAlbert", "Could not create directory for log files");
+                                            }
+/*
+                int permissionCheck = ContextCompat.checkSelfPermission(getContext().getApplicationContext().getCurrentActivity(),
+                        android.Manifest.permission.WRITE_CALENDAR);*/
+                                            try {
+                                                FileWriter filewriter = new FileWriter(logfile, true);
+                                                BufferedWriter bw = new BufferedWriter(filewriter);
+
+
+                                                // Write the string to the file
+                                                for (int i = 1; i < total_row; i++) {
+                                                    StringBuffer sb = new StringBuffer(data);
+                                                    sb.append("\n");
+                                                    bw.append(sb.toString());
+                                                }
+                                                bw.flush();
+                                                bw.close();
+                        /*Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                        emailIntent.setType("**");
+
+                        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+                                "SensorloggerAlbert data export");
+                        emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(logfile));
+                        getContext().startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+
+                        Log.i("SensorloggerAlbert", "export finished!");*/
+                                            } catch (IOException ioe) {
+                                                Log.e("BlueToothAlbert", "IOException while writing Logfile");
+                                            }
+                                           // dataList.clear();
                                         }
                                     });
                                 }
